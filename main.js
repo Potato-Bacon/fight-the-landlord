9b0b54cd1173f8c4e71538cc1e8b496090c65bbc
potatobacon <andylohbiz@gmail.com>
2022-08-12 01:52:03 +0800

    bug fixed

--- main.js
+++ main.js
@@ -2,60 +2,60 @@ import $ from "jquery";
 
 // declare card elements
 let deck = [
-  { Value: "14", Suit: "Spades" },
-  { Value: "15", Suit: "Spades" },
-  { Value: "3", Suit: "Spades" },
-  { Value: "4", Suit: "Spades" },
-  { Value: "5", Suit: "Spades" },
-  { Value: "6", Suit: "Spades" },
-  { Value: "7", Suit: "Spades" },
-  { Value: "8", Suit: "Spades" },
-  { Value: "9", Suit: "Spades" },
-  { Value: "10", Suit: "Spades" },
-  { Value: "11", Suit: "Spades" },
-  { Value: "12", Suit: "Spades" },
-  { Value: "13", Suit: "Spades" },
-  { Value: "14", Suit: "Diamonds" },
-  { Value: "15", Suit: "Diamonds" },
-  { Value: "3", Suit: "Diamonds" },
-  { Value: "4", Suit: "Diamonds" },
-  { Value: "5", Suit: "Diamonds" },
-  { Value: "6", Suit: "Diamonds" },
-  { Value: "7", Suit: "Diamonds" },
-  { Value: "8", Suit: "Diamonds" },
-  { Value: "9", Suit: "Diamonds" },
-  { Value: "10", Suit: "Diamonds" },
-  { Value: "11", Suit: "Diamonds" },
-  { Value: "12", Suit: "Diamonds" },
-  { Value: "13", Suit: "Diamonds" },
-  { Value: "14", Suit: "Club" },
-  { Value: "15", Suit: "Club" },
-  { Value: "3", Suit: "Club" },
-  { Value: "4", Suit: "Club" },
-  { Value: "5", Suit: "Club" },
-  { Value: "6", Suit: "Club" },
-  { Value: "7", Suit: "Club" },
-  { Value: "8", Suit: "Club" },
-  { Value: "9", Suit: "Club" },
-  { Value: "10", Suit: "Club" },
-  { Value: "11", Suit: "Club" },
-  { Value: "12", Suit: "Club" },
-  { Value: "13", Suit: "Club" },
-  { Value: "14", Suit: "Heart" },
-  { Value: "15", Suit: "Heart" },
-  { Value: "3", Suit: "Heart" },
-  { Value: "4", Suit: "Heart" },
-  { Value: "5", Suit: "Heart" },
-  { Value: "6", Suit: "Heart" },
-  { Value: "7", Suit: "Heart" },
-  { Value: "8", Suit: "Heart" },
-  { Value: "9", Suit: "Heart" },
-  { Value: "10", Suit: "Heart" },
-  { Value: "11", Suit: "Heart" },
-  { Value: "12", Suit: "Heart" },
-  { Value: "13", Suit: "Heart" },
-  { Value: "16", Suit: "Black" },
-  { Value: "16", Suit: "Red" },
+  { Value: 14, Suit: "Spades" },
+  { Value: 15, Suit: "Spades" },
+  { Value: 3, Suit: "Spades" },
+  { Value: 4, Suit: "Spades" },
+  { Value: 5, Suit: "Spades" },
+  { Value: 6, Suit: "Spades" },
+  { Value: 7, Suit: "Spades" },
+  { Value: 8, Suit: "Spades" },
+  { Value: 9, Suit: "Spades" },
+  { Value: 10, Suit: "Spades" },
+  { Value: 11, Suit: "Spades" },
+  { Value: 12, Suit: "Spades" },
+  { Value: 13, Suit: "Spades" },
+  { Value: 14, Suit: "Diamonds" },
+  { Value: 15, Suit: "Diamonds" },
+  { Value: 3, Suit: "Diamonds" },
+  { Value: 4, Suit: "Diamonds" },
+  { Value: 5, Suit: "Diamonds" },
+  { Value: 6, Suit: "Diamonds" },
+  { Value: 7, Suit: "Diamonds" },
+  { Value: 8, Suit: "Diamonds" },
+  { Value: 9, Suit: "Diamonds" },
+  { Value: 10, Suit: "Diamonds" },
+  { Value: 11, Suit: "Diamonds" },
+  { Value: 12, Suit: "Diamonds" },
+  { Value: 13, Suit: "Diamonds" },
+  { Value: 14, Suit: "Club" },
+  { Value: 15, Suit: "Club" },
+  { Value: 3, Suit: "Club" },
+  { Value: 4, Suit: "Club" },
+  { Value: 5, Suit: "Club" },
+  { Value: 6, Suit: "Club" },
+  { Value: 7, Suit: "Club" },
+  { Value: 8, Suit: "Club" },
+  { Value: 9, Suit: "Club" },
+  { Value: 10, Suit: "Club" },
+  { Value: 11, Suit: "Club" },
+  { Value: 12, Suit: "Club" },
+  { Value: 13, Suit: "Club" },
+  { Value: 14, Suit: "Heart" },
+  { Value: 15, Suit: "Heart" },
+  { Value: 3, Suit: "Heart" },
+  { Value: 4, Suit: "Heart" },
+  { Value: 5, Suit: "Heart" },
+  { Value: 6, Suit: "Heart" },
+  { Value: 7, Suit: "Heart" },
+  { Value: 8, Suit: "Heart" },
+  { Value: 9, Suit: "Heart" },
+  { Value: 10, Suit: "Heart" },
+  { Value: 11, Suit: "Heart" },
+  { Value: 12, Suit: "Heart" },
+  { Value: 13, Suit: "Heart" },
+  { Value: 16, Suit: "Black" },
+  { Value: 16, Suit: "Red" },
 ];
 
 // number of players
@@ -87,6 +87,24 @@ const shuffle = () => {
   }
 };
 
+const addOnClickEvent = ($card) => {
+  $card.on("click", (event) => {
+    let cardPlayerID = Number($card.attr("player"));
+    if (game.turn === cardPlayerID) {
+      $(event.currentTarget).toggleClass("shiftup");
+    }
+
+    if ($(event.currentTarget).hasClass("shiftup") === true) {
+      comboCheck.push(Number($card.attr("Value")));
+    } else {
+      comboCheck.pop();
+    }
+    console.log($(".shiftup").length, "length");
+    console.log(comboCheck, "comboCheck array");
+    comboCheck.sort((a, b) => a - b);
+    console.log(comboCheck, "sorted");
+  });
+};
 //push card from deck to landlord(bonus cards for landlordplayer for winning bidder)
 const deckToLandlord = () => {
   let $dealtCard = deck.shift();
@@ -97,6 +115,8 @@ const deckToLandlord = () => {
     .attr("id", $dealtCard.Suit + $dealtCard.Value)
     .attr("value", $dealtCard.Value);
   $(".landlord").append($card);
+
+  addOnClickEvent($card);
 };
 
 //function for deal all cards
@@ -122,23 +142,7 @@ const deckToHand = (index) => {
 
   $card.attr("player", index);
 
-  $card.on("click", (event) => {
-    let cardPlayerID = Number($card.attr("player"));
-    if (game.turn === cardPlayerID) {
-      $(event.currentTarget).toggleClass("shiftup");
-    }
-
-    if ($(event.currentTarget).hasClass("shiftup") === true) {
-      comboCheck.push($(".shiftup").attr("value"));
-    } else {
-      comboCheck.pop();
-    }
-
-    console.log($(".shiftup").length, "length");
-    console.log(comboCheck, "comboCheck array");
-    comboCheck.sort((a, b) => a - b);
-    console.log(comboCheck, "sorted");
-  });
+  addOnClickEvent($card);
 };
 shuffle();
 
@@ -157,6 +161,7 @@ const deal = () => {
     }
   }
 };
+
 deal();
 
 const biddingPhase = () => {
@@ -170,7 +175,6 @@ const biddingPhase = () => {
   $(".message").append($pass);
 
   let count = 1;
-
   $bidOne.on("click", () => {
     if (game.turn === 0);
     $bidOne.hide();
@@ -186,16 +190,6 @@ const biddingPhase = () => {
   });
 
   $bidThree.on("click", () => {
-    $(".landlordback").on("click", (event) => {
-      $(event.currentTarget).toggleClass("shiftup");
-
-      if ($(event.currentTarget).hasClass("shiftup") === true) {
-        comboCheck.push($(".shiftup").attr("value"));
-      } else {
-        comboCheck.pop();
-      }
-      console.log(comboCheck, "sorted");
-    });
     $(".landlordback").removeClass("landlordback").addClass("P1front");
     $("#hand1").append($(".P1front"));
 
@@ -209,8 +203,6 @@ const biddingPhase = () => {
     $("#turn").text(count);
   });
 };
-
-const landLordPlayer = () => {};
 
 let comboCheck = [];
 
@@ -248,26 +240,6 @@ const changeTurn = (index) => {
       .toggleClass("P" + index + "back");
   }
 };
-
-// const changeTurn = (index) => {
-//   index + 1;
-
-//   if (index > 3) {
-//     index = 1;
-//     let nextplayer = (game.turn = 1);
-//     $(".P" + index + "front").removeClass("shiftup");
-//     const playerHandCards = $("div#hand1>div.P" + index + "front");
-//     playerHandCards.addClass("P" + index + "back");
-//     // $(".P2back").toggleClass("P2front").toggleClass("P2back");
-//   } else {
-//     $(".P" + index + "front").removeClass("shiftup");
-//     $(".P1front").removeClass("shiftup");
-//     const playerHandCards = $("div#hand1>div.P" + index + "front");
-//     playerHandCards.addClass("P" + index + "back");
-//   }
-//   game.turn++;
-//   $(".P2back").toggleClass("P2front").toggleClass("P2back");
-// };
 
 let currentHighestCardValue = 0;
 let numCardsInPlay = 1;
@@ -313,6 +285,10 @@ $("#play").on("click", () => {
   }
 });
 
+$("#pass").on("click", () => {
+  changeTurn(game.turn);
+});
+
 //check for straights
 $("#play").on("click", () => {
   let num = 0;
@@ -326,7 +302,7 @@ $("#play").on("click", () => {
   for (let index = 0; index < comboCheck.length; index++) {
     let difference = comboCheck[index + 1] - comboCheck[index];
     if (
-      comboCheck.length >= 5 &&
+      $(".shiftup").length === 5 &&
       difference === 1 &&
       num > currentHighestCardValue
     ) {
@@ -334,12 +310,13 @@ $("#play").on("click", () => {
       numCardsInPlay = $(".shiftup").length;
       currentHighestCardValue = num;
       $(".playarea").append($(".shiftup"));
-      $(".P1front").removeClass("shiftup");
+      changeTurn(game.turn);
       comboCheck = [];
-      const playerOneHandCards = $("div#hand1>div.P1front");
-      playerOneHandCards.addClass("P1back");
-      game.turn++;
-      $(".P2back").toggleClass("P2front").toggleClass("P2back");
+      // $(".P1front").removeClass("shiftup");
+      // const playerOneHandCards = $("div#hand1>div.P1front");
+      // playerOneHandCards.addClass("P1back");
+      // game.turn++;
+      // $(".P2back").toggleClass("P2front").toggleClass("P2back");
     }
   }
 });
@@ -347,24 +324,40 @@ $("#play").on("click", () => {
 //check for triplets
 $("#play").on("click", () => {
   let isEqual = allNumbersEqual(comboCheck);
-  if ($(".shiftup").length === 3 && isEqual) {
+  if (
+    $(".shiftup").length === 3 &&
+    isEqual &&
+    comboCheck[0] > currentHighestCardValue
+  ) {
+    numCardsInPlay = $(".shiftup").length;
+    currentHighestCardValue = comboCheck[0];
     $(".playarea").append($(".shiftup"));
-    $(".P1front").removeClass("shiftup");
+    changeTurn(game.turn);
     comboCheck = [];
-    const playerOneHandCards = $("div#hand1>div.P1front");
-    playerOneHandCards.addClass("P1back");
+    // $(".P1front").removeClass("shiftup");
+    // comboCheck = [];
+    // const playerOneHandCards = $("div#hand1>div.P1front");
+    // playerOneHandCards.addClass("P1back");
   }
 });
 
 //check for bomb
 $("#play").on("click", () => {
   let isEqual = allNumbersEqual(comboCheck);
-  if ($(".shiftup").length === 4 && isEqual) {
+  if (
+    $(".shiftup").length === 4 &&
+    isEqual &&
+    comboCheck[0] > currentHighestCardValue
+  ) {
+    numCardsInPlay = $(".shiftup").length;
+    currentHighestCardValue = comboCheck[0];
     $(".playarea").append($(".shiftup"));
-    $(".P1front").removeClass("shiftup");
+    changeTurn(game.turn);
     comboCheck = [];
-    const playerOneHandCards = $("div#hand1>div.P1front");
-    playerOneHandCards.addClass("P1back");
+    // $(".P1front").removeClass("shiftup");
+    // comboCheck = [];
+    // const playerOneHandCards = $("div#hand1>div.P1front");
+    // playerOneHandCards.addClass("P1back");
   }
 });
 
@@ -376,10 +369,11 @@ $("#play").on("click", () => {
     $(".shiftup").eq(1).attr("value") === "16"
   ) {
     $(".playarea").append($(".shiftup"));
-    $(".P1front").removeClass("shiftup");
+    changeTurn(game.turn);
     comboCheck = [];
-    const playerOneHandCards = $("div#hand1>div.P1front");
-    playerOneHandCards.addClass("P1back");
+    // comboCheck = [];
+    // const playerOneHandCards = $("div#hand1>div.P1front");
+    // playerOneHandCards.addClass("P1back");
   }
 });
 
@@ -392,7 +386,6 @@ $("#play").on("click", () => {
 
   if ($(".shiftup").length === 5 && isTriple && isPair) {
     $(".playarea").append($(".shiftup"));
-    $(".P1front").removeClass("shiftup");
     comboCheck = [];
     const playerOneHandCards = $("div#hand1>div.P1front");
     playerOneHandCards.addClass("P1back");
